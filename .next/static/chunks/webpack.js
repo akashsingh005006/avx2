66f57f9154703836874d2bb2a4845a699e86959b
Om Satapathy <omsatapathy05@gmail.com>
2024-06-18 20:09:30 +0530

    fixed the problem of frontend not connecting and big numberish type error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b622ad35acd1bd0"; }
+/******/ 		__webpack_require__.h = function() { return "bfe177383cdfb334"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
